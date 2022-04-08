28a789d1c6bcc22978ea61e9b5f26d6662a8dd44
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-04-08 08:32:28 -0300

    Started the server construction part.

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const server = express()
+
+
+//request, response
+server.get("/", (request, response) => {
+    return response.send('Hello WWWorld');
+})
+
+server.listen(3000, () => {console.log("Server running...")})
