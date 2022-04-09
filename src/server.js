0b1ab190a97d0d07e18382244f2550438832e405
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-04-08 23:10:34 -0300

    white spce

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const server = express()
 
-
 //request, response
 server.get("/", (request, response) => {
     return response.send('Hello WWWorld');
 })
 
-server.listen(3000, () => {console.log("Server running...")})
+server.listen(3000, () => {
+    console.log("Server running...")
+})
