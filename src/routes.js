426dbba96c743a0a76be72d2bd9533c7088c0b48
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-04-19 11:17:40 -0300

    Added object to simulate data from db, using through the route to profile.ejs file

--- src/routes.js
+++ src/routes.js
@@ -21,10 +21,19 @@ routes.get('/profile', (req,res) => res.sendFile(basePath + "/profile.html"))
 
 //EJS can read views when its locate in the root, need to adjust when is in "src"
 const views = __dirname + "/views/"
+
+//data to fill fields
+const profile = {name: "Mayk",
+avatar: "https://avatars.githubusercontent.com/u/6643122?v=4",
+"monthly-budget": 3000,
+"days-per-week": 5,
+"hours-per-day": 5,
+"vacation-per-year": 4}
+
 routes.get('/', (req, res) => res.render(views + "index"))
 routes.get('/job', (req, res) => res.render(views + "job"))
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
-routes.get('/profile', (req, res) => res.render(views + "profile"))
+routes.get('/profile', (req, res) => res.render(views + "profile", {profile:profile}))
 
 //Manually redirecting to a html page/file
 //routes.get('/index.html', (req,res) => {return res.redirect('/') })
