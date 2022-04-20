86d456c69a18f10405a071dd2dfd3747c1377193
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-04-20 12:15:09 -0300

    Adjusted the route "/job" (post) to fill information in the job object. Added variables to fill the fields form temporary. Added a function "remainingDays" to process the days data and inserted the function to the route "/" (get). Added to the job object more properties, such as status and reamining days (using spread syntax).

--- src/routes.js
+++ src/routes.js
@@ -23,20 +23,90 @@ routes.get('/profile', (req,res) => res.sendFile(basePath + "/profile.html"))
 const views = __dirname + "/views/"
 
 //data to fill fields
-const profile = {name: "Mayk",
-avatar: "https://avatars.githubusercontent.com/u/6643122?v=4",
-"monthly-budget": 3000,
-"days-per-week": 5,
-"hours-per-day": 5,
-"vacation-per-year": 4}
+const profile = {
+    name: "Mayk",
+    avatar: "https://github.com/maykbrito.png",
+    "monthly-budget": 3000,
+    "days-per-week": 5,
+    "hours-per-day": 5,
+    "vacation-per-year": 4,
+    "hour-value": 75
+}
 
-routes.get('/', (req, res) => res.render(views + "index"))
+//jobs controller
+const jobs = [
+    {
+        id: 1,
+        name: "Pizzaria Guloso",
+        'daily-hours': 2,
+        'total-hours': 1,
+        createdAt: Date.now(), // adding new date
+    },
+    {
+        id: 2,
+        name: "OneTwo Project",
+        'daily-hours': 3,
+        'total-hours': 47,
+        createdAt: Date.now(), // adding new date
+    }
+]
+
+function remainingDays(job) {
+    //calc time remaining
+    const daysLeft = (job["total-hours"] / job["daily-hours"]).toFixed()
+
+    const createdDate = new Date(job.createdAt)
+    const dueDay = createdDate.getDate() + Number(daysLeft)
+    const dueDateInMs = createdDate.setDate(dueDay)
+
+    const timeDiffInMs = dueDateInMs - Date.now()
+
+    //converting ms to days
+    const dayInMs = 1000 * 60 ** 2 * 24
+
+    //remaining days
+    const dayDiff = Math.floor(timeDiffInMs / dayInMs)
+
+    return dayDiff
+
+}
+routes.get('/', (req, res) => {
+    const updatedJobs = jobs.map((job) => {
+        //job adjustments
+        const remaining = remainingDays(job)
+        const status = remaining <= 0 ? 'done' : 'progress'
+        return {
+            
+            ...job,//Spread syntax
+            remaining,
+            status,
+            budget: profile["hour-value"] * job["total-hours"]
+        }
+    })
+
+
+
+    return res.render(views + "index", { jobs : updatedJobs})
+})
 routes.get('/job', (req, res) => res.render(views + "job"))
 routes.post('/job', (req, res) => {
-    console.log("save data")
+    //console.log("save data")
+    //console.log(req.body)
+
+    //get last ID if exists or consider number 1
+    const lastId = jobs[jobs.length - 1]?.id || 1
+    jobs.push({
+        id: lastId + 1,
+        name: req.body.name,
+        'daily-hours': req.body["daily-hours"],
+        'total-hours': req.body["total-hours"],
+        createdAt: Date.now() // adding new date
+    }
+    )
+    return res.redirect('/')
 })
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
-routes.get('/profile', (req, res) => res.render(views + "profile", {profile:profile}))
+routes.get('/profile', (req, res) => res.render(views + "profile", { profile: profile }))
 
 //Manually redirecting to a html page/file
 //routes.get('/index.html', (req,res) => {return res.redirect('/') })
