baef9166b25cacf82716eb42cede271a5dd45477
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-04-26 19:44:31 -0300

    Added object status for the dashboard (index)

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -2,24 +2,35 @@ const Job = require('../model/Job')
 const JobUtils = require('../utils/JobUtils')
 const Profile = require('../model/Profile')
 
+
 module.exports = {
     index(req, res) {
         const jobs = Job.get();
         const profile = Profile.get();
+
+        let statusCount = {
+            progress: 0,
+            done: 0,
+            total: jobs.length,
+        }
 
         const updatedJobs = jobs.map((job) => {
             //job adjustments
             const remaining = JobUtils.remainingDays(job)
             const status = remaining <= 0 ? 'done' : 'progress'
 
-            return {
+            //when variable status receive "done" or "progress" it will add +1 one of itens into statusCount
+            //statusCount[done] += 1
+            //statusCount[progress] += 1
+            statusCount[status] += 1
 
+            return {
                 ...job,//Spread syntax
                 remaining,
                 status,
                 budget: JobUtils.calculateBudget(job, profile["hour-value"])
             }
         })
-        return res.render("index", { jobs: updatedJobs })
+        return res.render("index", { jobs: updatedJobs, profile: profile, statusCount: statusCount })
     },
 }
