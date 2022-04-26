03e655da1790e4707ac3846b2f9b566d839ee876
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-04-26 12:31:31 -0300

    Added model for the Profile creating/exporting data

---
+++ src/model/Profile.js
@@ -0,0 +1,21 @@
+const { update } = require("../controllers/ProfileController");
+
+//data to fill fields
+let data = {
+    name: "Mayk",
+    avatar: "https://github.com/maykbrito.png",
+    "monthly-budget": 3000,
+    "days-per-week": 5,
+    "hours-per-day": 5,
+    "vacation-per-year": 4,
+    "hour-value": 75
+}
+
+module.exports = {
+    get(){
+        return data;
+    },
+    update(newData){
+        data = newData;
+    }
+}
