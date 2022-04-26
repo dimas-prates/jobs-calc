c31dd5ab8ecedc8068e32b46758f8d3261d066d1
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-04-26 11:47:24 -0300

    Added new folder and fille "src/controllers/ProfileController.js" with content from "src/routes.js" to continue refactoring

---
+++ src/controllers/ProfileController.js
@@ -0,0 +1,32 @@
+module.exports ={
+    index(req, res) {
+        return res.render("profile", { profile: Profile.data })
+    },
+    update(req, res) {
+        //req.body to have data
+        const data = req.body
+
+        //define weeks per year: 52
+        const weeksPerYear = 52
+
+        //remove vacation weeks per year
+        //how much hours per week working
+        const weeksPerMonth = (weeksPerYear - data["vacation-per-year"]) / 12
+
+        //total work hours monthly
+        const weekTotalHours = data["hours-per-day"] * data["days-per-week"]
+
+        //hours per month
+        const monthlyTotalHours = weekTotalHours * weeksPerMonth
+
+        //hour value
+        const hourValue = data["monthly-budget"] / monthlyTotalHours
+
+        Profile.data = {
+            ...Profile.data,
+            ...req.body,
+            "hour-value": hourValue
+        }
+        return res.redirect('/profile')
+    }
+}
