0f4b2fd567324128232f8a30fb795a4ada547218
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-04-27 11:50:05 -0300

    Removed static data and setup to work with sqlite

--- src/model/Profile.js
+++ src/model/Profile.js
@@ -1,21 +1,50 @@
-const { update } = require("../controllers/ProfileController");
+//const { update } = require("../controllers/ProfileController");
+const Database = require('../db/config')
 
 //data to fill fields
-let data = {
-    name: "Mayk",
-    avatar: "https://github.com/maykbrito.png",
-    "monthly-budget": 3000,
-    "days-per-week": 5,
-    "hours-per-day": 5,
-    "vacation-per-year": 4,
-    "hour-value": 75
-}
+// let data = {
+//     name: "Mayk",
+//     avatar: "https://github.com/maykbrito.png",
+//     "monthly-budget": 3000,
+//     "days-per-week": 5,
+//     "hours-per-day": 5,
+//     "vacation-per-year": 4,
+//     "hour-value": 75
+// }
 
 module.exports = {
-    get() {
-        return data;
+    async get() {
+        const db = await Database()
+        
+        //const  data2 = await db.run(`SELECt * FROM profile`)
+        const  data = await db.get(`SELECt * FROM profile`)
+        
+        await db.close()
+        //console.log(data2)
+        //return data2;
+
+        return {
+            name: data.name,
+            avatar: data.avatar,
+            "monthly-budget": data.monthly_budget,
+            "days-per-week": data.days_per_week,
+            "hours-per-day": data.hours_per_day,
+            "vacation-per-year": data.vacation_per_year,
+            "hour-value": data.hour_value 
+        }
     },
-    update(newData) {
-        data = newData;
+    async update(newData) {
+        const db = await Database();
+
+        db.run(`UPDATE profile SET 
+        name = "${newData.name}",
+        avatar = "${newData.avatar}",
+        monthly_budget = ${newData["monthly-budget"]},
+        days_per_week = ${newData["days-per-week"]},
+        hours_per_day = ${newData["hours-per-day"]},
+        vacation_per_year= ${newData["vacation-per-year"]},
+        hour_value= ${newData["hour-value"]}
+        `)
+        await db.close()
     }
 }
