31759fc8a4006b1c2d457967f1411123942d488d
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-04-27 10:30:52 -0300

    Implemented "async/await" so the db can work properly

--- src/db/init.js
+++ src/db/init.js
@@ -1,66 +1,69 @@
 const Database = require('./config')
 
-//Opening db
-Database()
+const initDb = {
+    async init() {
+        //Opening db
+        const db = await Database()
+        await db.exec(`CREATE TABLE profile (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                avatar TEXT,
+                monthly_budget INT,
+                days_per_week INT,
+                hours_per_day INT,
+                vacation_per_year INT,
+                hour_value INT
+            )`);
+        await db.exec(` CREATE TABLE jobs(
+                id  INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                daily_hours INT,
+                total_hours INT,
+                createdAt DATETIME
+            )`);
 
-Database.exec(`CREATE TABLE profile (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    avatar TEXT,
-    monthly_budget INT,
-    days_per_week INT,
-    hours_per_day INT,
-    vacation_per_year INT,
-    hour_value INT
-)`);
-Database.exec(` CREATE TABLE jobs(
-    id  INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    daily_hours INT,
-    total_hours INT,
-    createdAt DATETIME
-)`);
+        await db.run(`INSERT INTO profile (
+                name,
+                avatar,
+                monthly_budget,
+                days_per_week,
+                hours_per_day,
+                vacation_per_year,
+                hour_value
+            ) VALUES (
+                "Mayk",
+                "https://github.com/maykbrito.png",
+                3000,
+                5,
+                5,
+                4,
+                75
+            )`);
 
-Database.run(`INSERT INTO profile (
-    name,
-    avatar,
-    monthly_budget,
-    days_per_week,
-    hours_per_day,
-    vacation_per_year,
-    hour_value
-) VALUES (
-    "Mayk",
-    "https://github.com/maykbrito.png",
-    3000,
-    5,
-    5,
-    4,
-    75
-)`);
+        await db.run(`INSERT INTO jobs (
+                name,
+                daily_hours,
+                total_hours,
+                createdAt
+            ) VALUES (
+                "Pizzaria Guloso",
+                2,
+                1,
+                1652162486
+            );`)
 
-Database.run(`INSERT INTO jobs (
-    name,
-    dayly_hours,
-    total_hours,
-    createdAt,
-) VALUES (
-    "Pizzaria Guloso",
-    2,
-    1,
-    1652162486
-);`)
-
-Database.run(`INSERT INTO jobs (
-    name,
-    dayly_hours,
-    total_hours,
-    createdAt,
-) VALUES (
-    "OneTwo Project",
-    3,
-    47,
-    1651062585
-);`)
-
-Database.close()
+        await db.run(`INSERT INTO jobs (
+                name,
+                daily_hours,
+                total_hours,
+                createdAt
+            ) VALUES (
+                "OneTwo Project",
+                3,
+                47,
+                1651062585
+            );`)
+        await db.close()
+    }
+}
+initDb.init()
