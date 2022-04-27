c3ef8049e328d7a5faa755d540e533b5c624587a
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-04-27 12:22:15 -0300

    Adapted to use "async/await"

--- src/controllers/JobController.js
+++ src/controllers/JobController.js
@@ -6,16 +6,16 @@ module.exports = {
     create(req, res) {
         return res.render("job")
     },
-    save(req, res) {
-        const jobs = Job.get();
+    async save(req, res) {
+        // const jobs = await Job.get();
 
         //console.log("save data")
         //console.log(req.body)
         //get last ID if exists or consider number 1
         //optional chaining operator "?."
-        const lastId = jobs[jobs.length - 1]?.id || 1
-        Job.create({
-            id: lastId + 1,
+        // const lastId = jobs[jobs.length - 1]?.id || 1
+        await Job.create({
+            // id: lastId + 1,
             name: req.body.name,
             'daily-hours': req.body["daily-hours"],
             'total-hours': req.body["total-hours"],
@@ -23,24 +23,24 @@ module.exports = {
         })
         return res.redirect('/')
     },
-    show(req, res) {
+    async show(req, res) {
         const jobId = req.params.jobId
 
-        const jobs = Job.get();
+        const jobs = await Job.get();
 
         const job = jobs.find(job => Number(job.id) === Number(jobId))
         if (!job) {
             return res.send('Job not found')
         }
 
-        const profile = Profile.get();
+        const profile = await Profile.get();
         job.budget = JobUtils.calculateBudget(job, profile["hour-value"])
 
         return res.render("job-edit", { job })
     },
-    update(req, res) {
+    async update(req, res) {
         const jobId = req.params.jobId
-        const jobs = Job.get();
+        const jobs = await Job.get();
 
         const job = jobs.find(job => Number(job.id) === Number(jobId))
         if (!job) {
